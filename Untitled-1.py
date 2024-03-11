9a7e25e23e4c14add12bb62066469148d3f326d3
Zarigata <vallestrauch@yahoo.com>
2024-03-11 15:33:04 -0300

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+# Make me a llama test code
+
+llama = {
+  "name": "Llama",
+    "age": 5,
+    "speaks": [" llamada ", " maaaaahhhh"],
+    "eats": ["grass", " hay"],
+    "likes": ["pasture", "treats"]
+}
+
+print(llama["name"]) # Output: Llama
+print(llama["age"]) # Output: 5
+print(llama["speaks"]) # Output: llamada, maaaaahhhh
+print(llama["eats"]) # Output: grass, hay
+print(llama["likes"]) # Output: pasture, treats
